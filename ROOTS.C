a409832bcc8525a24b02d5a38cbd190a3620fdb5
sivapullaiah <mangalisivapllaiah@gmail.com>
2023-01-30 15:55:28 +0530

    sum of digits ,roots and factors.c was debuged

--- ROOTS.C
+++ ROOTS.C
@@ -1,40 +1,26 @@
-	/* Write a C program toe find the roots of a quadratic equation. */
+/* Write a C program toe find the roots of a quadratic equation. */
 
 #include<stdio.h>
+#include<conio.h>
 #include<math.h>
 
 void main()
 {
 int a,b,c,root1,root2;
+clrscr();
 printf("\n Enter values of a,b,c for finding roots of a quadratic eq:\n");
 scanf("%d%d%d",&a,&b,&c);
 
 /*checking condition*/
-int D=(b*b)-(4*a*c);
-//printf("%d",D);
-if(D==0)
+if(b*b-4*a*c>=0)
 {
-	root1=-b/(2*a);
-	root2=root1;
-	 printf("\n*****ROOTS ARE*****\n");
- 	printf("\n root1=%d\n root2=%d",root1,root2);
-}
-else if(D>0)
-{
-printf("%d", sqrt(D));
- root1=-(b)+sqrt(D)/(2*a);
- root2=-b-sqrt(D)/(2*a);
+ root1=(-b+sqrt(b*b-(4*a*c)))/(2*a);
+ root2=(-b-sqrt(b*b-(4*a*c)))/(2*a);
  printf("\n*****ROOTS ARE*****\n");
  printf("\n root1=%d\n root2=%d",root1,root2);
 }
-
 else
-{
  printf("\n Imaginary Roots.");
-int img = -sqrt(D)/(2*a);
-int img1 = -b/(2*a);
- printf("\n*****ROOTS ARE*****\n");
- printf("\n root1=%d + i (%d)\n root2=%d - i(%d)", img1,img,img1,img);
+ getch();
+ }
 
-}
-}
